8eca9260071317f5562c3298e34f016ec0df410d
Matteo Cavestri <matteo.cavestri@cavestri.net>
2024-07-23 17:48:14 +0200

    disable md exporter

--- config/plugins/neorg.nix
+++ config/plugins/neorg.nix
@@ -13,8 +13,8 @@
       "core.itero".__empty = null;
       "core.ui.calendar".__empty = null;
       "core.promo".__empty = null;
-      "core.export".__empty = null;
-      "core.export.markdown".config.extensions = "all";
+      #"core.export".__empty = null;
+      #"core.export.markdown".config.extensions = "all";
       "core.pivot".__empty = null;
       "core.keybinds".config = {
         default_keybinds = true;
