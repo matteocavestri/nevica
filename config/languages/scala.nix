8de9ba865cf3e271b65d7e59d0300adcc2ac9689
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-08-24 03:42:26 +0200

    updated

--- config/languages/scala.nix
+++ config/languages/scala.nix
@@ -16,47 +16,23 @@
     };
   };
   extraConfigLua = ''
-      require("neotest").setup({
-      adapters = {
-        require("neotest-scala")({
-            -- Command line arguments for runner
-            -- Can also be a function to return dynamic values
-            args = {"--no-color"},
-            -- Runner to use. Will use bloop by default.
-            -- Can be a function to return dynamic value.
-            -- For backwards compatibility, it also tries to read the vim-test scala config.
-            -- Possibly values bloop|sbt.
-            runner = "sbt",
-            -- Test framework to use. Will use utest by default.
-            -- Can be a function to return dynamic value.
-            -- Possibly values utest|munit|scalatest.
-            framework = "scalatest",
-        })
-      }
-    })
+    require("neotest").setup({
+    adapters = {
+      require("neotest-scala")({
+          -- Command line arguments for runner
+          -- Can also be a function to return dynamic values
+          args = {"--no-color"},
+          -- Runner to use. Will use bloop by default.
+          -- Can be a function to return dynamic value.
+          -- For backwards compatibility, it also tries to read the vim-test scala config.
+          -- Possibly values bloop|sbt.
+          runner = "sbt",
+          -- Test framework to use. Will use utest by default.
+          -- Can be a function to return dynamic value.
+          -- Possibly values utest|munit|scalatest.
+          framework = "scalatest",
+      })
+     }
+     })
   '';
-  # extraConfigLua = ''
-  #   local dap = require("dap")
-  #   dap.configurations.scala = {
-  #     {
-  #       type = "scala",
-  #       request = "launch",
-  #       name = "RunOrTest",
-  #       metals = {
-  #         runType = "runOrTestFile",
-  #       },
-  #     },
-  #     {
-  #       type = "scala",
-  #       request = "launch",
-  #       name = "Test Target",
-  #       metals = {
-  #         runType = "testTarget",
-  #       },
-  #     },
-  #   }
-  #   metals_config.on_attach = function(client, bufnr)
-  #     require("metals").setup_dap()
-  #   end
-  # '';
 }
